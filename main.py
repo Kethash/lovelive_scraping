a7ab71eaa68fa5faed55ab7858d0cf7273f3b97d
Kethash <ketessar.tan@gmail.com>
2024-09-01 23:48:20 +0200

    Groups with urls

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from functools import reduce
 from typing import Generator
 from bs4 import BeautifulSoup
 import requests
@@ -11,17 +12,17 @@ r = requests.get(f"{BASE_URL}/wiki/Songs_BPM_List")
 
 print("Fetching songs urls")
 
-songs_urls = extract_page_urls(BeautifulSoup(r.text, features="html.parser"))
+group_name_and_page_urls: list[tuple[str, str]] = extract_page_urls(BeautifulSoup(r.text, features="html.parser"))
 
 print("Beginning scraping !")
-print(f"There are {len(songs_urls)} titles to scrap !")
+print(f"There are {len(group_name_and_page_urls)} titles to scrap !")
 
 output = open("output.csv", "w")
-output.write("title,audio_url,link\n")
+output.write("group,title,audio_url,link\n")
 output.close()
 
 print("Writing to csv")
-def run_multi(song_url: str):
+def run_multi(group_name_and_page_url: str):
     """run_multi
     -----
     Run scraping into multiple processes
@@ -30,15 +31,15 @@ def run_multi(song_url: str):
     """
 
     # Fetching audios sources in the song page
-    titles_and_audios: Generator[tuple[str, str, str], None, None] = get_titles_and_audios(f"{BASE_URL}/{song_url}")
+    titles_and_audios: Generator[tuple[str, str, str], None, None] = get_titles_and_audios(f"{BASE_URL}/{group_name_and_page_url[1]}")
 
     # Writing the outputs in csv
     with open("output.csv", 'a') as f:
         for ta in titles_and_audios:
-            f.write(f'"{ta[0]}","{ta[1]}","{ta[2]}"\n')
+            f.write(f'"{group_name_and_page_url[0]}","{ta[0]}","{ta[1]}","{ta[2]}"\n')
 
 # Running in a pool with the size of the number of cpus inside the machine
 with Pool(os.cpu_count()) as p:
-    p.map(run_multi, songs_urls)
+    p.map(run_multi, group_name_and_page_urls)
 
 print("Done ! Written in output.csv")
