0bc2e27cc9bbf78c754a006293fe94312c7a9835
Kethash <ketessar.tan@gmail.com>
2024-09-01 14:49:37 +0200

    fix

--- functions.py
+++ functions.py
@@ -7,6 +7,14 @@ from bs4 import BeautifulSoup
 
 
 def get_titles_and_audios(url: str) -> Generator[tuple[str, str, str], None, None]:
+    """get_titles_and_audios
+    -----
+    Fetch the titles and the audio source of a song in its page
+    Params:
+        url (str): The url of the song
+    Returns:
+        urls_and_sources (Generator[tuple[str,str,str], None, None])
+    """
 
     try:
         title_page = requests.get(url)
@@ -21,6 +29,14 @@ def get_titles_and_audios(url: str) -> Generator[tuple[str, str, str], None, Non
 
 
 def extract_rows(tables: list) -> list:
+    """extract_rows
+    -----
+    Extracts the rows of the scraped table
+    Params:
+        tables (list): The HTML tables
+    Returns:
+        tables_rows (list): The scraped HTML table rows
+    """
     tables_rows = [table.find_all('td') for table in tables]
     tables_rows = reduce(lambda x, y: x + y, tables_rows)
     idx = 0
@@ -33,10 +49,26 @@ def extract_rows(tables: list) -> list:
     return tables_rows
 
 def extract_titles(tables_rows: list) -> list:
+    """extract_titles
+    -----
+    Extract the titles
+    Params:
+        tables_rows (list): The list of the extracted tables
+    Returns:
+        titles (list): The titles
+    """
     titles = [title.text for title in tables_rows][1::4]
     return titles
 
 def extract_audio_urls(tables_rows: list) -> list:
+    """extract_audio_urls
+    -----
+    Extract the audio source urls
+    Params:
+        tables_rows (list): The list of the extracted tables
+    Returns:
+        audio_urls (list): The audio source urls
+    """
     audios_urls = []
     for audio in tables_rows[3::4]:
         if audio.find('audio') is not None:
@@ -46,6 +78,14 @@ def extract_audio_urls(tables_rows: list) -> list:
     return audios_urls
 
 def extract_page_urls(soup: BeautifulSoup) -> list:
+    """extract_page_urls
+    -----
+    Scrapes the page by its url
+    Params:
+        soup (BeautifulSoup): The soup of the page
+    Returns:
+        titles_urls (list): The urls of the titles
+    """
     tables = soup.find_all("table")
 
     table_rows = [table.find_all('tr') for table in tables]
--- main.py
+++ main.py
@@ -8,24 +8,35 @@ from multiprocessing import Pool
 BASE_URL = "https://love-live.fandom.com"
 
 r = requests.get(f"{BASE_URL}/wiki/Songs_BPM_List")
-titles_urls = extract_page_urls(BeautifulSoup(r.text, features="html.parser"))
+
+print("Fetching songs urls")
+
+songs_urls = extract_page_urls(BeautifulSoup(r.text, features="html.parser"))
 
 print("Beginning scraping !")
-print(f"There are {len(titles_urls)} titles to scrap !")
+print(f"There are {len(songs_urls)} titles to scrap !")
 
 output = open("output.csv", "w")
 output.write("title,audio_url,link\n")
 output.close()
 
 print("Writing to csv")
-def run_multi(title_url: str):
+def run_multi(song_url: str):
+    """run_multi
+    -----
+    Run scraping into multiple processes
+    Params:
+        title_url (str): The url of the song
+    """
 
+    # Fetching audios sources in the song page
+    titles_and_audios: Generator[tuple[str, str, str], None, None] = get_titles_and_audios(f"{BASE_URL}/{song_url}")
 
-    titles_and_audios: Generator[tuple[str, str, str], None, None] = get_titles_and_audios(f"{BASE_URL}/{title_url}")
-
+    # Writing the outputs in csv
     with open("output.csv", 'a') as f:
         for ta in titles_and_audios:
             f.write(f'"{ta[0]}","{ta[1]}","{ta[2]}"\n')
 
+# Running in a pool with the size of the number of cpus inside the machine
 with Pool(os.cpu_count()) as p:
-    p.map(run_multi, titles_urls)
+    p.map(run_multi, songs_urls)
